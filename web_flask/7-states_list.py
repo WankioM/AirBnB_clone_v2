109e0fbf7b5615a5abccd0c4ac0dc0c905767c96
WankioM <111064857+WankioM@users.noreply.github.com>
2023-04-24 01:15:37 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+Update FileStorage: (models/engine/file_storage.py)
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
+Add a public method def close(self):: call reload() method for deserializing the JSON file to objects
+Update DBStorage: (models/engine/db_storage.py)
+
+Add a public method def close(self):: call remove() method on the private session attribute (self.__session) tips or close() on the class Session tips
+Update State: (models/state.py) - If it’s not already present
+
+If your storage engine is not DBStorage, add a public getter method cities to return the list of City objects from storage linked to the current State
 """
 from models import storage
 from flask import Flask
